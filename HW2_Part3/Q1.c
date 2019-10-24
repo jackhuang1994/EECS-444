ba7206ac077affe66b2116118dd8862987443a75
jackhuang1994 <56372703+jackhuang1994@users.noreply.github.com>
2019-10-24 17:32:44 -0400

    Update Q1.c

--- HW2_Part3/Q1.c
+++ HW2_Part3/Q1.c
@@ -2,7 +2,7 @@
 
 int main() {
 	int variable[3] = {3, 5, 0};
-    variable[2] = (variable[0]*variable[1] - variable[0]/2);
+    	variable[2] = (variable[0]*variable[1] - variable[0]/2);
 	printf("%d ", variable[2]);
 	return 0;
 }
